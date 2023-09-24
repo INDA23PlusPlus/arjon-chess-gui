da38c5e672b1343d9187607beee07a7d0d95d46c
Arvid Jonasson <jonassonarvid02@gmail.com>
2023-09-24 21:08:24 +0200

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 use ggez::conf::{FullscreenType, NumSamples, WindowMode, WindowSetup};
-use ggez::graphics::Transform;
+use ggez::graphics::{Canvas, DrawMode, Image, Mesh, Rect, Transform};
 use ggez::{event, graphics, Context, GameResult};
 use jonathan_hallstrom_chess::{Board, Color, Move};
 use mint::{Point2, Vector2};
 use std::cmp::min;
 use std::collections::HashMap;
+
+#[macro_use]
+extern crate lazy_static;
+
+static PIECES_IMAGE_BYTES: &'static [u8] = include_bytes!("Pieces.png");
+
+lazy_static! {
+    static ref CHESSBOARD_MESH: graphics::MeshBuilder = {
+        let mut mesh = graphics::MeshBuilder::new();
+        let white = graphics::Color::new(1.0, 0.9, 0.9, 1.0);
+        let black = graphics::Color::new(0.9, 0.7, 0.7, 1.0);
+        for row in 0..8usize {
+            for col in 0..8usize {
+                mesh.rectangle(
+                    DrawMode::fill(),
+                    graphics::Rect::new(
+                        col as f32 / 8.0f32,
+                        row as f32 / 8.0f32,
+                        1.0f32 / 8.0f32,
+                        1.0f32 / 8.0f32,
+                    ),
+                    match (row + col) % 2 == 0 {
+                        true => white,
+                        false => black,
+                    },
+                )
+                .unwrap();
+            }
+        }
+        mesh
+    };
+}
 
 #[derive(Eq, PartialEq, Copy, Clone, Hash)]
 enum Square {
@@ -101,20 +133,22 @@ fn parse_moves(moves: Vec<Move>) -> [[HashMap<(usize, usize), Vec<Move>>; 8]; 8]
     parsed
 }
 
-enum PromotablePieces {
-    Queen,
-    Knight,
-    Bishop,
-    Rook,
-    NA,
-}
 struct Game {
+    // Game logic
     board: Board,
+
+    // Rendering aid
     squares: [[Square; 8]; 8],
     legal_moves: [[HashMap<(usize, usize), Vec<Move>>; 8]; 8],
     selected_from: Option<(usize, usize)>,
     selected_to: Option<(usize, usize)>,
-    pieces_image: graphics::Image,
+
+    // Rendering
+    pieces_image: Image,
+    chessboard_mesh: Mesh,
+    promotion_mesh: Mesh,
+    selected_piece_mesh: Mesh,
+    available_move_mesh: Mesh,
 }
 
 impl Game {
@@ -134,7 +168,171 @@ impl Game {
             legal_moves,
             selected_from: None,
             selected_to: None,
-            pieces_image: graphics::Image::from_bytes(ctx, include_bytes!("Pieces.png")).unwrap(),
+            pieces_image: Image::from_bytes(ctx, PIECES_IMAGE_BYTES).unwrap(),
+            chessboard_mesh: Mesh::from_data(ctx, CHESSBOARD_MESH.build().clone()),
+            promotion_mesh: Mesh::new_rectangle(
+                ctx,
+                DrawMode::fill(),
+                Rect::new(0.0, 0.0, 1.0, 1.0),
+                graphics::Color::new(0.0f32, 0.0f32, 0.0f32, 0.75f32),
+            )
+            .unwrap(),
+            selected_piece_mesh: Mesh::new_rectangle(
+                ctx,
+                DrawMode::fill(),
+                Rect::new(0.0f32, 0.0f32, 1.0f32 / 8.0f32, 1.0f32 / 8.0f32),
+                graphics::Color::new(0.0f32, 0.5f32, 0.0f32, 0.75f32),
+            )
+            .unwrap(),
+            available_move_mesh: Mesh::new_circle(
+                ctx,
+                DrawMode::fill(),
+                Point2 {
+                    x: 0.5f32 / 8.0f32,
+                    y: 0.5f32 / 8.0f32,
+                },
+                0.25f32 / 8.0f32,
+                0.25f32 / (8.0f32 * 1024.0f32),
+                graphics::Color::new(0.0, 0.5, 0.0, 0.75),
+            )
+            .unwrap(),
+        }
+    }
+    fn draw_squares(&self, canvas: &mut Canvas) {
+        let (width, height) = {
+            let cords = canvas.screen_coordinates().unwrap();
+            (cords.w, cords.h)
+        };
+
+        canvas.draw(
+            &self.chessboard_mesh,
+            graphics::DrawParam::default().scale(Vector2 {
+                x: width,
+                y: height,
+            }),
+        );
+    }
+
+    fn draw_piece(&self, canvas: &mut Canvas, piece: &Square, row: usize, col: usize) {
+        if *piece == Square::Empty {
+            return;
+        }
+        let color = piece.color().unwrap();
+
+        let rect = Rect::new(
+            match piece {
+                Square::Pawn(_) => 5.0,
+                Square::Rook(_) => 4.0,
+                Square::Knight(_) => 3.0,
+                Square::Bishop(_) => 2.0,
+                Square::Queen(_) => 1.0,
+                _ => 0.0,
+            } / 6.0,
+            match color {
+                Color::White => 0.0,
+                Color::Black => 1.0,
+            } / 2.0,
+            1.0 / 6.0,
+            1.0 / 2.0,
+        );
+
+        let (width, height) = {
+            let cords = canvas.screen_coordinates().unwrap();
+            (cords.w, cords.h)
+        };
+
+        canvas.draw(
+            &self.pieces_image,
+            graphics::DrawParam {
+                src: rect,
+                color: graphics::Color::WHITE,
+                transform: Transform::Values {
+                    dest: Point2 {
+                        x: col as f32 * width / 8.0f32,
+                        y: row as f32 * height / 8.0f32,
+                    },
+                    rotation: 0.0,
+                    scale: Vector2 {
+                        x: (width * 6.0f32) / (self.pieces_image.width() as f32 * 8.0f32),
+                        y: (height * 2.0f32) / (self.pieces_image.height() as f32 * 8.0f32),
+                    },
+                    offset: Point2 { x: 0.0, y: 0.0 },
+                },
+                z: 0,
+            },
+        );
+    }
+
+    fn draw_pieces(&self, canvas: &mut Canvas) {
+        for row in 0..8usize {
+            for col in 0..8usize {
+                // Draw piece on current square
+                self.draw_piece(canvas, &self.squares[row][col], row, col);
+            }
+        }
+    }
+    fn draw_promotion_selection(&self, canvas: &mut Canvas, row: usize, col: usize) {
+        let (width, height) = {
+            let cords = canvas.screen_coordinates().unwrap();
+            (cords.w, cords.h)
+        };
+
+        // Grey out the chessboard
+        canvas.draw(
+            &self.promotion_mesh,
+            graphics::DrawParam::default().scale(Vector2 {
+                x: width,
+                y: height,
+            }),
+        );
+        let dir = match row {
+            7 => -1isize,
+            0 => 1isize,
+            _ => panic!("Promotion not on last row"),
+        };
+
+        let (from_row, from_col) = self.selected_from.unwrap();
+
+        let color = self.squares[from_row][from_col].color().unwrap();
+        let pieces: [Square; 4] = [
+            Square::Queen(color),
+            Square::Knight(color),
+            Square::Rook(color),
+            Square::Bishop(color),
+        ];
+
+        for dy in 0..4usize {
+            let row = (row as isize + dir * dy as isize) as usize;
+            self.draw_piece(canvas, &pieces[dy], row, col);
+        }
+    }
+
+    fn draw_move_selection(&self, canvas: &mut Canvas, row: usize, col: usize) {
+        let (width, height) = {
+            let cords = canvas.screen_coordinates().unwrap();
+            (cords.w, cords.h)
+        };
+
+        canvas.draw(
+            &self.selected_piece_mesh,
+            graphics::DrawParam::default().dest_rect(Rect {
+                x: (col as f32) * width / 8.0f32,
+                y: (row as f32) * height / 8.0f32,
+                w: width,
+                h: height,
+            }),
+        );
+
+        for ((row, col), _) in &self.legal_moves[row][col] {
+            canvas.draw(
+                &self.available_move_mesh,
+                graphics::DrawParam::default().dest_rect(Rect {
+                    x: (*col as f32) * width / 8.0f32,
+                    y: (*row as f32) * height / 8.0f32,
+                    w: width,
+                    h: height,
+                }),
+            );
         }
     }
 }
@@ -145,141 +343,18 @@ impl event::EventHandler for Game {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas = graphics::Canvas::from_frame(ctx, graphics::Color::WHITE);
+        let mut canvas = Canvas::from_frame(ctx, graphics::Color::WHITE);
 
-        // Draw board squares
-        for row in 0..8usize {
-            for col in 0..8usize {
-                let color = if (row + col) % 2 == 0 {
-                    graphics::Color::new(1.0, 0.9, 0.9, 1.0)
-                } else {
-                    graphics::Color::new(0.9, 0.7, 0.7, 1.0)
-                };
-                let rect = graphics::Mesh::new_rectangle(
-                    ctx,
-                    graphics::DrawMode::fill(),
-                    graphics::Rect::new_i32((col * 100) as i32, (row * 100) as i32, 100, 100),
-                    color,
-                )?;
-                canvas.draw(&rect, graphics::DrawParam::default());
-
-                // Draw piece on current square
-                if self.squares[row][col] != Square::Empty {
-                    let piece = &self.squares[row][col];
-                    let color = piece.color().unwrap();
-
-                    let rect = graphics::Rect::new(
-                        match piece {
-                            Square::Pawn(_) => 5.0,
-                            Square::Rook(_) => 4.0,
-                            Square::Knight(_) => 3.0,
-                            Square::Bishop(_) => 2.0,
-                            Square::Queen(_) => 1.0,
-                            _ => 0.0,
-                        } / 6.0,
-                        match color {
-                            Color::White => 0.0,
-                            Color::Black => 1.0,
-                        } / 2.0,
-                        1.0 / 6.0,
-                        1.0 / 2.0,
-                    );
-                    canvas.draw(
-                        &self.pieces_image,
-                        graphics::DrawParam {
-                            src: rect,
-                            color: graphics::Color::WHITE,
-                            transform: Transform::Values {
-                                dest: Point2 {
-                                    x: (col * 100) as f32,
-                                    y: (row * 100) as f32,
-                                },
-                                rotation: 0.0,
-                                scale: Vector2 { x: 1.0, y: 1.0 },
-                                offset: Point2 { x: 0.0, y: 0.0 },
-                            },
-                            z: 0,
-                        },
-                    );
-                }
-            }
-        }
+        self.draw_squares(&mut canvas);
+        self.draw_pieces(&mut canvas);
 
         // Draw selection for promotion
         if let Some((row, col)) = self.selected_to {
-            // Grey out the arena
-            let greyed = graphics::Color::new(0.0, 0.0, 0.0, 0.75);
-            let rect = graphics::Mesh::new_rectangle(
-                ctx,
-                graphics::DrawMode::fill(),
-                graphics::Rect::new_i32(0, 0, 800, 800),
-                greyed,
-            )?;
-            canvas.draw(&rect, graphics::DrawParam::default());
-            let dir = match row {
-                7 => -1isize,
-                0 => 1isize,
-                _ => panic!("Promotion not on last row"),
-            };
-
-            let (from_row, from_col) = self.selected_from.unwrap();
-
-            let color = self.squares[from_row][from_col].color().unwrap();
-
-            for dy in 0..4 {
-                let rect = graphics::Rect::new(
-                    (1.0 + dy as f32) / 6.0,
-                    match color {
-                        Color::White => 0.0,
-                        Color::Black => 1.0,
-                    } / 2.0,
-                    1.0 / 6.0,
-                    1.0 / 2.0,
-                );
-                canvas.draw(
-                    &self.pieces_image,
-                    graphics::DrawParam {
-                        src: rect,
-                        color: graphics::Color::WHITE,
-                        transform: Transform::Values {
-                            dest: Point2 {
-                                x: (col * 100) as f32,
-                                y: ((row as isize + dy * dir) * 100) as f32,
-                            },
-                            rotation: 0.0,
-                            scale: Vector2 { x: 1.0, y: 1.0 },
-                            offset: Point2 { x: 0.0, y: 0.0 },
-                        },
-                        z: 0,
-                    },
-                );
-            }
+            self.draw_promotion_selection(&mut canvas, row, col);
         }
         // Draw available moves
         else if let Some((row, col)) = self.selected_from {
-            let color = graphics::Color::new(0.0, 0.5, 0.0, 0.75);
-            let rect = graphics::Mesh::new_rectangle(
-                ctx,
-                graphics::DrawMode::fill(),
-                graphics::Rect::new_i32((col * 100) as i32, (row * 100) as i32, 100, 100),
-                color,
-            )?;
-            canvas.draw(&rect, graphics::DrawParam::default());
-
-            for ((row, col), _) in &self.legal_moves[row][col] {
-                let circle = graphics::Mesh::new_circle(
-                    ctx,
-                    graphics::DrawMode::fill(),
-                    Point2 {
-                        x: (col * 100 + 50) as f32,
-                        y: (row * 100 + 50) as f32,
-                    },
-                    25.0,
-                    0.1,
-                    color,
-                )?;
-                canvas.draw(&circle, graphics::DrawParam::default());
-            }
+            self.draw_move_selection(&mut canvas, row, col);
         }
 
         canvas.finish(ctx)
@@ -287,7 +362,7 @@ impl event::EventHandler for Game {
 
     fn mouse_button_down_event(
         &mut self,
-        ctx: &mut Context,
+        _ctx: &mut Context,
         _button: event::MouseButton,
         x: f32,
         y: f32,
@@ -309,9 +384,9 @@ impl event::EventHandler for Game {
                     (self.selected_to.as_ref().unwrap().0 as isize - row as isize).abs() as usize;
                 let promotion_piece = match drow {
                     0 => jonathan_hallstrom_chess::PieceType::Queen,
-                    1 => jonathan_hallstrom_chess::PieceType::Bishop,
-                    2 => jonathan_hallstrom_chess::PieceType::Knight,
-                    3 => jonathan_hallstrom_chess::PieceType::Rook,
+                    1 => jonathan_hallstrom_chess::PieceType::Knight,
+                    2 => jonathan_hallstrom_chess::PieceType::Rook,
+                    3 => jonathan_hallstrom_chess::PieceType::Bishop,
                     _ => panic!("Couldn't select a promotion piece."),
                 };
                 let mut move_made = false;
